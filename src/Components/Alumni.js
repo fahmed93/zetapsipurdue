b8ae4d37123cc46573ac2fbd020a82a55dd37909
Fahad Ahmed <fahmed7x@walgreenco.net>
2019-04-05 11:04:13 -0500

    Alumni page image and text

--- src/Components/Alumni.js
+++ src/Components/Alumni.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-
+import './../styles/alumni.css';
 class Alumni extends Component {
   render() {
-    return <div />;
+    return (
+      <React.Fragment>
+        <h1 className="Page-header">Alumni</h1>
+        <img
+          className="img"
+          src="/images/alumni/brothers50th.jpg"
+          alt="50th Anniversary Celebration"
+        />
+        <div className="col-sm-8">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
